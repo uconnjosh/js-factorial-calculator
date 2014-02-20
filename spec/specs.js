c2cfe51fd0cd83d753385d2651916256360db54d
Joshua T Paul & Aaron K Williamson <student@epicodus.com>
2014-02-20 10:52:43 -0800

    Factorial Final

---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+describe('factorial0', function () {
+  it('if the input is 0, a 1 should be returned', function () {
+    factorial0(0).should.equal(1);
+  });
+  it('should take a number and return its factorial', function() {
+    factorialLesson(5).should.equal(120);
+  });
+});
