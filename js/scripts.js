10b0358788a986d8cfe9dd4aaec33c74f4b61475
Joshua T Paul & Aaron K Williamson <student@epicodus.com>
2014-02-20 16:03:26 -0800

    Factorial Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
-//Josh and Aaron
-var factorial0 = function(inputNumber) {
-  return 1;
+var recursion = function(number) {
+  number = parseFloat(number);
+  if (number === 0) {
+    return 1;
+  } else if (number < 0) {
+    alert('You need a positive number to compute factorials');
+    return
+  } else {
+    return (number) * (recursion(number - 1));
+  }
 };
 
-var factorialLesson = function(inputNumber) {
-  var counter = (inputNumber - 1);
-  
-  while (counter > 0) {  
-    inputNumber = (inputNumber * counter);
-    counter = counter -= 1;
-}
-return inputNumber;
-};
-//modification
-//HELLO WORLD!!
+$(document).ready(function() {
+  $("form#factorial").submit(function(event) {
+    number = $("input#inputNumber").val();
+    
+    var result = recursion(number);
+    $("#result").text(result);
+    event.preventDefault();
+  });
+});
